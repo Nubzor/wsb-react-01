6d78dc517c66f0df4f6fcfa333e5a5cd2b6ee6e1
Mateusz Marzęcki <mmarzecki@atlassian.com>
2022-04-09 13:29:08 +0200

    Dodanie styli do komponentu Href

--- src/Href.js
+++ src/Href.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './href.css';
+
 function Href(props) {
     return <a
-        className="App-link"
+        className="App-link hrefColor hrefSize"
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
