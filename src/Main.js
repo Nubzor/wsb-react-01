5179c0aa895b564449e73cd368a6914ff1a06640
Mateusz Marzęcki <mmarzecki@atlassian.com>
2022-04-09 14:01:04 +0200

    Dodanie renderowania listy osob za pomoca .map w komponencie Main

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
+const people = [
+    {
+        name: 'Ala',
+        surname: 'Alowata',
+        age: 26,
+    },
+    {
+        name: 'Ola',
+        surname: 'Olowata',
+        age: 43,
+    },
+    {
+        name: 'Jan',
+        surname: 'Niezbędny',
+        age: 18,
+    },
+    {
+        name: 'Piotr',
+        surname: 'Potrzebny',
+        age: 30,
+    },
+    {
+        name: 'Ela',
+        surname: 'Elowata',
+        age: 34,
+    },
+]
+
 class Main extends React.Component {
     render() {
         return <main>
-            
+            <ol>
+                { people.map(function(person) {
+                    return <li>{person.name}</li>
+                }) }
+            </ol>
         </main>
     }
 }
