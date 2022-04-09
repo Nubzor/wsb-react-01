9abce537fb20aee9b43b4efd12d8717baf9db774
Mateusz Marzęcki <mmarzecki@atlassian.com>
2022-04-09 13:18:39 +0200

    Dodanie styli do komponentu Paragraph

--- src/Paragraph.js
+++ src/Paragraph.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './paragraph.css';
+
 // props is always an object !!!
 // props.text
 
 function Paragraph(props) {
-    return <p>
+    return <p className="paragraph">
         <h1>{ props.text }</h1>
         { props.subTitle ? <h4>{ props.subTitle }</h4> : null }
     </p>
